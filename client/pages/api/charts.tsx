999a841ec325bca31dbf051dc45df2419c114c4b
avonderg <77714730+avonderg@users.noreply.github.com>
2024-02-03 14:53:49 -0500

    create chart method taking in data from frontend

---
+++ client/pages/api/charts.tsx
@@ -0,0 +1,19 @@
+import { WithId, Document } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+
+export default async (
+  req: any,
+  res: { json: (arg0: WithId<Document>[]) => void }
+) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("charts");
+
+    const chart = await db.collection("chart").find({}).toArray();
+
+    const chartAsJson = res.json(chart);
+    console.log(chartAsJson);
+  } catch (e) {
+    console.error(e);
+  }
+};
