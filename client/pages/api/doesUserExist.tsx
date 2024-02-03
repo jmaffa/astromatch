6eeea4671c36f8eb058664c9a5c3c2e8d2f7a72c
avonderg <77714730+avonderg@users.noreply.github.com>
2024-02-03 18:03:06 -0500

    user exists method

---
+++ client/pages/api/doesUserExist.tsx
@@ -0,0 +1,29 @@
+import { WithId, Document } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+
+export default async (
+  req: any,
+  res: {
+    json: (
+      arg0:
+        | WithId<Document>[]
+        | { insertedId: Object }
+        | { error: string }
+        | { exists: boolean }
+    ) => void;
+  }
+) => {
+  try {
+    const client = await clientPromise;
+    const db = client.db("charts");
+
+    const { email } = req.params; // Extract the parameter
+
+    const existingUser = await db.collection("chart").findOne({ email });
+    const exists = !!existingUser; // boolean
+    res.json({ exists });
+  } catch (e) {
+    console.error(e);
+    res.json({ error: "An error occurred while fetching data" });
+  }
+};
